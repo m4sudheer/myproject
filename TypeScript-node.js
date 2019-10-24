7622b92205ad3490883506b8c800c2916309de68
m4sudheer <43779288+m4sudheer@users.noreply.github.com>
2019-10-24 11:08:26 +0530

    Update TypeScript-node.js

--- TypeScript-node.js
+++ TypeScript-node.js
@@ -9,3 +9,11 @@ rootDir:
 "moduleResolution": "node"
 
 npm i -D typescript ts-node nodemon @types/node @types/express
+
+
+package.json
+"scripts": {
+    "start": "node dest/app.js",
+    "dev" : "nodemon src/app.ts",
+    "build" : "tsc -p ."
+  },
